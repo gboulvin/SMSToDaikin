dd7626a4e506c5f97c43c64871975043b3d7d229
gboulvin <78817935+gboulvin@users.noreply.github.com>
2021-11-03 17:49:44 +0100

    Add files via upload

---
+++ Reception_SMS_IR_Daikin.ino
@@ -0,0 +1,192 @@
+/* Allumer une LED par SMS
+Pour allumer la LED envoyer led on
+pour éteindre la LED envoyer led off
+*/
+
+/* 1) Zone 1 : les déclarations */
+// 1.a) Les bibliothèques et création d'objets
+
+#include <SoftwareSerial.h>
+#include <DHT.h>
+#define DHTPIN 4
+#define DHTTYPE DHT22      // DHT 22
+DHT dht(DHTPIN, DHTTYPE);  // Création de l'objet dht
+#include <DYIRDaikin.h>
+int Txd = 6, Rxd = 7; // SIM800
+SoftwareSerial gsm(Txd, Rxd); // RX, TX : il faut relier Rx de l'Arduino au Tx du SIM800
+
+#define DYIRDAIKIN_SOFT_IR
+DYIRDaikin irdaikin;
+//int isOn=0;
+
+
+// 1.c) Les variables globales
+int led=3;
+String reponse, numeroSMS, SMS;
+unsigned long t0;
+
+/* 2) Zone 2 : Initialisation (le setup) */
+void setup() {
+  pinMode(led,OUTPUT);
+  Serial.begin(9600);
+  gsm.begin(9600);
+  delay(2000);
+  dht.begin();
+  Serial.println("Initialisation...");
+  gsm.println("AT");
+  while (!message("OK",1000,0)) gsm.println("AT");
+  gsm.println("AT+CNUM");            // Affiche n° de la carte SIM utilisée
+  message("OK",20000,1);
+  Serial.print ("Qualite reseau : ");
+  gsm.println("AT+CSQ");            // Qualité du réseau, pb si CSQ = 0
+  message("OK",10000,1);
+  gsm.println("AT+CMGF=1");         // Mode Texte
+  message("OK",1000,0);
+  gsm.println("AT+CMGD=1,4"); // effacer les SMS en mémoire dans la carte SIM
+  message("OK",2000,0);      // car on lit toujours le message N°1 de la carte SIM...
+
+#ifdef DYIRDAIKIN_SOFT_IR
+  irdaikin.begin(3);
+  #else
+  irdaikin.begin();
+  #endif
+  }
+
+void Chauffage(){
+  irdaikin.on();
+  irdaikin.setSwing_off();
+  irdaikin.setMode(3);
+  irdaikin.setFan(5);//FAN speed to MAX
+  irdaikin.setTemp(18);
+  //----everything is ok to execute send command-----
+  irdaikin.sendCommand(); 
+}
+
+void Airco(){
+  irdaikin.on();
+  irdaikin.setSwing_on();
+  irdaikin.setMode(0);
+  irdaikin.setFan(5);//FAN speed to MAX
+  irdaikin.setTemp(20);
+  //----everything is ok to execute send command-----
+  
+  irdaikin.sendCommand();
+  
+}
+
+void AllOff(){
+  irdaikin.off();
+   
+  //----everything is ok to execute send command-----
+  irdaikin.sendCommand();
+  
+}
+
+void Confort(){
+  irdaikin.on();
+  irdaikin.setSwing_off();
+  irdaikin.setMode(3);
+  irdaikin.setFan(4);//FAN speed to presque MAX
+  irdaikin.setTemp(24);
+  //----everything is ok to execute send command-----
+  irdaikin.sendCommand();
+}
+
+/* 3) Zone 3 : le Programme Principal */
+void loop() {
+  if (message("+CMTI:",20000,0)) LireSMS();   // Si nouveau SMS disponible SIM800 envoie +CMTI:
+}
+
+/* 4) Zone 4 : les sous programmes (ou fonctions) */
+
+void LireSMS(){
+  gsm.println("AT+CMGF=1");  // Mode Texte
+  message("OK",1000,0);
+  gsm.println("AT+CMGR=1");  // Lit le premier message disponible sur la carte SIM
+  message("OK",2000,1);
+  // Récupérer N° de telephone emetteur pour lui répondre
+  int test=reponse.indexOf("+32");
+  numeroSMS = reponse.substring(test,test+12);
+  Serial.println("SMS recu depuis : " + numeroSMS);
+  
+ // Analyse du message reçu 
+  
+  if (reponse.indexOf("Chauffage")>0) {
+     SMS = "Ordre recu : Allumer le chauffage a 18 degres !";
+     Chauffage();
+     }
+     else {
+       if (reponse.indexOf("Off")>0){
+       SMS = "Ordre recu : Eteindre tout !";
+       AllOff();
+       }
+       else {
+         if (reponse.indexOf("Airco")>0){
+         SMS = "Ordre recu : Allumer airco a 20 degres !";
+         Airco();
+         }
+         else {
+          if (reponse.indexOf("Confort")>0){
+          SMS = "Ordre recu : Allumer chauffage a 24 degres !";
+          Confort();
+          }
+         else {
+          if (reponse.indexOf("Temp")>0){
+          float h = dht.readHumidity();
+          float t = dht.readTemperature();
+          if (isnan(h) || isnan(t) ){
+             Serial.println(F("Impossible de se connecter au capteur DHT !"));
+             }
+          Serial.println("Température "+String(t,1)+" C");
+          Serial.println("Humidité "+String(h,1)+" %");
+          SMS = "La temperature actuelle est de "+String(t,1)+" C" + " avec une humidite de "+String(h,1)+ " pourcents";   //A string to regroup the whole message as it's composed of Strings and Float --> to a single string,
+          }
+          else {
+            SMS = "Je n'ai pas compris !";
+          }
+        }
+         }
+      }
+    }
+  gsm.println("AT+CMGD=1,4"); // effacer les SMS de la Carte SIM
+  message("OK",1000,0);
+  Serial.print("Message à envoyer : ");
+  Serial.println(SMS);
+  
+  // Envoyer la confirmation de l'ordre par SMS 
+  Serial.println("Envoi du message de confirmation"); 
+  gsm.println("AT+CMGS=\""+ numeroSMS +"\"");
+  message(">",1000,0);
+  gsm.print(SMS);
+  gsm.write(26); // Caractère de fin 26 <Ctrl-Z>
+  gsm.println(""); 
+  message("+CMGS:",10000,1);  
+}
+      
+/* Sous Programme message(attente, timeout, affiche)
+Après avoir envoyé une instruction AT au module SIM800,celui-ci répond. 
+message("OK", 1000,1) :
+  Vérifie que la réponse contient le terme "OK" (attente)
+  Il attend au maximum 1000ms (timeout) la réponse
+  Affiche (affiche=1) la réponse et le temps mis pour l'obtenir
+  ou n'affiche pas (affiche=0) sauf si la réponse attendue n'est pas correcte
+  renvoie vrai si le message contient OK en moins de 1000 ms
+*/
+
+boolean message(String attente, unsigned int timeout, boolean affiche) {
+  t0 = millis();
+  reponse="";
+  while (millis() - t0 < timeout) {
+    while(gsm.available()) reponse.concat(char(gsm.read()));
+    if (reponse.indexOf(attente)>0){        // Lit encore 100 ms le port série  
+      delay (100);                          // pour être sur de ne rien louper...
+       while(gsm.available()) reponse.concat(char(gsm.read()));
+        break;}}
+  if (affiche || reponse.indexOf(attente)==-1 ) {      
+    Serial.print("Attente = "+ attente +" "+ reponse.indexOf(attente)+" duree ");
+    Serial.print(millis()-t0); Serial.println(" ms");
+    Serial.println(reponse);}
+  
+  if (reponse.indexOf(attente)>0) return true;
+  else return false;
+}
